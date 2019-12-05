02a0693e49531666108147af4c35904b6586b1a8
martinmalinda <malindacz@gmail.com>
2019-12-05 09:38:41 +0100

    add some gravity experiments

--- src/scripts/land.js
+++ src/scripts/land.js
@@ -9,10 +9,8 @@ function addStar(leftPct, topPct) {
     scale = 0.2;
   }
 
-  newStarEl.setAttribute('style',
-    `top:${topPct}vh;
-   left:${leftPct}vw;
-   transform:scale(${scale}, ${scale})`);
+  newStarEl.style.top = `${topPct}vh`;
+  newStarEl.style.left = `${leftPct}vw`;
   graphics.prepend(newStarEl);
 }
 
@@ -23,19 +21,72 @@ graphics.addEventListener('click', event => {
   addStar(leftPct, topPct);
 });
 
-Array(100).fill(0).forEach(() => {
+Array(10).fill(0).forEach(() => {
   const leftPct = Math.random() * 100;
   const topPct = Math.random() * 100;
   addStar(leftPct, topPct);
 });
 
+function getLiveStars() {
+  return Array.from(document.querySelectorAll('.star:not(.fallen)'));
+}
+
 // random star fall
 (function starFall() {
-  const unfallenStars = Array.from(document.querySelectorAll('.star:not(.fallen)'));
-  const randomIndex = Math.round(Math.random() * unfallenStars.length);
-  const randomStar = unfallenStars[randomIndex];
+  const liveStars = getLiveStars();
+  const randomIndex = Math.round(Math.random() * liveStars.length);
+  const randomStar = liveStars[randomIndex];
 
-  randomStar.classList.add('fallen');
+  if (randomStar) {
+    randomStar.classList.add('fallen');
+  }
 
-  window.setTimeout(starFall, Math.round(Math.random() * 15000));
+  window.setTimeout(starFall, Math.round(Math.random() * 300));
+
 })();
+
+function getCoordinates(star) {
+  return star.getBoundingClientRect();
+}
+
+function getDelta(star1, star2) {
+  const { left: x1, top: y1 } = getCoordinates(star1);
+  const { left: x2, top: y2 } = getCoordinates(star2);
+
+  return { top: y2 - y1, left: x2 - x1 };
+}
+
+function getDistance(star1, star2) {
+  const { top, left } = getDelta(star1, star2);
+  return Math.sqrt(Math.pow(left, 2) - Math.pow(top, 2));
+}
+
+function getClosestStar(star, stars) {
+  const distances = stars
+    .filter(iteratedStar => iteratedStar !== star)
+    .map(iteratedStar => {
+      return {
+        star: iteratedStar,
+        distance: getDistance(star, iteratedStar)
+      }
+    }).sort((a, b) => b.distance - a.distance);
+  return distances[0].star;
+}
+
+function moveStar(star, top, left) {
+  const coordinates = getCoordinates(star);
+  star.setAttribute('style', `
+    left: ${coordinates.x + left}px;
+    top: ${coordinates.y + top}px;
+  `);
+}
+
+// // Gravity!
+// window.setInterval(() => {
+//   const liveStars = getLiveStars();
+//   liveStars.forEach(star => {
+//     const closestStar = getClosestStar(star, liveStars);
+//     const delta = getDelta(star, closestStar);
+//     moveStar(star, (1000 / delta.top) / 100, (1000 / delta.left) / 100);
+//   });
+// }, 300);
