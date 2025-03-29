0c4e559ed05624afecf35db8673a0796708ae80f
Martin Malinda <martinmalinda@172-15-173-51.lightspeed.mssnks.sbcglobal.net>
2025-03-29 17:58:40 +0000

    add about section

--- pages/index/index.page.vue
+++ pages/index/index.page.vue
@@ -234,7 +234,7 @@ const webPortfolio = [powersave, herohero, goout, leadfeeder];
           target="_blank"
           :icon="Vue"
           variant="secondary"
-          size="small"
+          size="large"
         />
         <Button
           href="https://supabase.com/"
@@ -252,6 +252,8 @@ const webPortfolio = [powersave, herohero, goout, leadfeeder];
         />
       </div>
       <p>
+        Although my main focus is automation, I keep my web development skills
+        sharp. <br />
         My expertise lies in building web user interfaces that load fast and
         stay fast. <br />
         I have a lot of experience with Vue.js, TypeScript and building complex
@@ -299,6 +301,63 @@ const webPortfolio = [powersave, herohero, goout, leadfeeder];
         </div>
       </div>
     </Container>
+    <div class="mt-8"></div>
+    <Container class="block">
+      <h2>About</h2>
+      <p class="about-text">
+        <img
+          src="/teaching.png"
+          width="300"
+          height="300"
+          style="
+            border-radius: 50%;
+            float: right;
+            shape-outside: circle();
+            -webkit-shape-outside: circle(); /* For Safari */
+            margin-left: 1rem;
+            margin-bottom: 1rem;
+            margin-top: 2rem;
+            display: block;
+          "
+          alt="Teaching"
+          class="teaching-image"
+        />
+        I learned to code to build community websites for MMORPG games when I
+        was 12 and have played around with web tech ever since. I got a bachelor
+        degree in
+        <a
+          href="https://natur.cuni.cz/en/admissions/study-programmes/doctoral-studies/environmental-science"
+          target="_blank"
+          >environmental science</a
+        >
+        but started a tech career right after. At
+        <a href="https://www.topmonks.com/index.html" target="_blank"
+          >TopMonks</a
+        >, I built JS-heavy webapps and later committed to building the UI for
+        <a href="https://www.leadfeeder.com" target="_blank">Leadfeeder</a>. I
+        also taught programming and web development at
+        <a href="https://www.instagram.com/greenfox_official/" target="_blank"
+          >Green Fox Academy</a
+        >
+        and created a state-of-the-art, highly optimized event discovery website
+        for
+        <a href="https://goout.net/" target="_blank">GoOut</a> — a key ticketing
+        service in the Czech Republic. There, I met the people with whom I
+        co-founded
+        <a href="https://about.herohero.co/en/" target="_blank">Herohero</a> — a
+        monetization platform for creators that now dominates the creator
+        economy market in the Czech Republic and Slovakia, outcompeting Patreon
+        and others.
+        <br />
+        Currently, I'm focused on freelance automation development & consulting,
+        which allows me to collaborate closely with clients, learn from each
+        other, and apply AI in pragmatic ways. On the side, I'm building
+        <a href="https://powersave.pro" target="_blank">Powersave</a> - a chrome
+        extension to save to your database ASAP. <br />
+        When I'm away from screens, I like to surround myself with nature,
+        history and art.
+      </p>
+    </Container>
     <div class="mt-10 py-5">
       <footer>
         <Container>
@@ -330,6 +389,20 @@ h2 {
 
   @media (max-width: 900px) {
     font-size: 25px;
+  }
+}
+
+.about-text {
+  a {
+    display: inline-block;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.5);
+  }
+}
+
+.teaching-image {
+  @media (max-width: 600px) {
+    width: 180px;
+    height: 180px;
   }
 }
 
