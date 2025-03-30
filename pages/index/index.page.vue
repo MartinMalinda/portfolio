104ba6603e2c671e6f286d2ce4029582fe6bb674
Martin Malinda <martinmalinda@unknown6ee7c039323d.lan>
2025-03-30 11:04:18 +0100

    add writing

--- pages/index/index.page.vue
+++ pages/index/index.page.vue
@@ -358,6 +358,133 @@ const webPortfolio = [powersave, herohero, goout, leadfeeder];
         history and art.
       </p>
     </Container>
+    <Container class="block">
+      <h2>Writing</h2>
+      <ul class="writing">
+        <li>
+          <a
+            href="https://martinmalinda.medium.com/imagining-a-universal-workflow-automation-2c17c6907377"
+            target="_blank"
+          >
+            Imagining a universal workflow automation
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://medium.com/js-dojo/lazy-rendering-in-vue-to-improve-performance-dcccd445d5f"
+            target="_blank"
+          >
+            Lazy rendering in Vue to improve performance
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/feed/update/urn:li:activity:7310975020497829954/"
+            target="_blank"
+          >
+            Validation fields in Airtable
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/posts/martin-malinda-58b03253_heres-my-current-productivity-stack-todoist-activity-7295866065731907584-1tDc"
+            target="_blank"
+          >
+            My current productivity stack
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/posts/martin-malinda-58b03253_heres-an-airtable-trick-how-you-can-significantly-activity-7290332082159939586-K0u1"
+            target="_blank"
+          >
+            Airtable automations reacting to bulk changes
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/posts/martin-malinda-58b03253_heres-one-trick-how-to-use-airtable-ai-fields-activity-7281344724592975872-G-Of"
+            target="_blank"
+          >
+            Effective AI analysis in Airtable
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/posts/martin-malinda-58b03253_things-to-check-about-a-public-api-before-activity-7275233377480253440-QiCL"
+            target="_blank"
+          >
+            API quality evaluation
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/posts/martin-malinda-58b03253_vcard-activity-7269370769225175040-_iia"
+            target="_blank"
+          >
+            How to add a contact from Airtable to your phone ASAP
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/posts/martin-malinda-58b03253_airtable-email-clients-activity-7268736015073222656-_PK7"
+            target="_blank"
+          >
+            A formula to open email client with dynamic content from Airtable
+            ASAP
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/posts/martin-malinda-58b03253_if-you-have-checklists-that-you-want-activity-7266910744263168000-rrZ7"
+            target="_blank"
+          >
+            An alternative approach to checklists in Airtable
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/posts/martin-malinda-58b03253_when-if-and-how-to-use-custom-search-activity-7263553177101193216--8pf"
+            target="_blank"
+          >
+            Effective use of Chrome via custom search engines
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/posts/martin-malinda-58b03253_airtable-add-to-calendar-button-activity-7243952014890553344-k4pV"
+            target="_blank"
+          >
+            A formula button in Airtable to add an event to google calendar
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/posts/martin-malinda-58b03253_portfolio-website-activity-7242832515948810241-GOt8"
+            target="_blank"
+          >
+            How I built this website
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/posts/martin-malinda-58b03253_today-i-checked-how-major-workflow-automation-activity-7235602071930998784-xNt1"
+            target="_blank"
+          >
+            How do well known workflow platforms compare when it comes to
+            writing code
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/posts/martin-malinda-58b03253_ive-improved-my-flow-for-billable-hours-activity-7214913446994182148-g-wj"
+            target="_blank"
+          >
+            How I'm tracking billable hours in Airtable
+          </a>
+        </li>
+      </ul>
+    </Container>
     <div class="mt-10 py-5">
       <footer>
         <Container>
@@ -392,10 +519,20 @@ h2 {
   }
 }
 
-.about-text {
+.about-text,
+.writing {
   a {
     display: inline-block;
-    border-bottom: 1px solid rgba(0, 0, 0, 0.5);
+    border-bottom: 1px solid rgba(0, 0, 0, 0.3);
+    padding: 2px 4px;
+    background: rgba(0, 0, 0, 0.03);
+    transition: 0.05s all;
+
+    &:hover {
+      background: black;
+      color: white;
+      border-radius: 5px;
+    }
   }
 }
 
